33f69b4275f65f5f4d63e32c139ca9e13f242a69
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-06 21:47:37 +0900

    feat: Set MAC ADRESS

--- cogs/fairy.py
+++ cogs/fairy.py
@@ -10,7 +10,33 @@ class Fairy(Cog):
     @slash_command(name='지성이있나요?')
     async def fairy(self, ctx):
         """컬방에 지성이의 유무를 알려줍니다."""
-        url = ''
+        
+        # 이지성 KERT_5G 기기별 MAC ADRESS
+        # 핸드폰:   2A:BC:84:94:87:9C
+        # 노트북:   1C:C1:0C:E2:54:78
+        # 아이패드: 3E:57:55:B5:7A:8A
+        
+        # 이지성 KERT_2G 기기별 MAC ADRESS        
+        # 핸드폰:   EE:A6:85:AE:31:2C
+        # 노트북:   1C:C1:0C:E2:54:78
+        # 아이패드: 56:98:A2:57:F4:C3
+        
+        url = 'http://router.asus.com/Main_Login.asp'
 
 def setup(bot):
     bot.add_cog(Fairy(bot))
+    
+# import requests
+# from bs4 import BeautifulSoup
+
+# URL = 'http://router.asus.com/Main_Login.asp'
+# LOGIN_DATA = {
+#     'Username': 'Kert', 
+#     'Password': 'password123'
+# }
+
+# session = requests.session()
+# response = session.get(URL, data=LOGIN_DATA)
+# response.raise_for_status()
+
+# soup = BeautifulSoup(response.text, 'html.parser')
