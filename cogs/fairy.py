c0be53ac3dcf37a7c4cba2fd6f7501ef62d8419e
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-06 16:39:16 +0900

    feat: Set fairy command

--- cogs/fairy.py
+++ cogs/fairy.py
@@ -1,2 +1,16 @@
-# 이지성
-# 컬방에 지성이 있나요?
+import discord
+import asyncio
+from discord.ext.commands import Cog
+from discord.commands import slash_command
+
+class Fairy(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name='지성이있나요?')
+    async def fairy(self, ctx):
+        """컬방에 지성이의 유무를 알려줍니다."""
+        url = ''
+
+def setup(bot):
+    bot.add_cog(Fairy(bot))
