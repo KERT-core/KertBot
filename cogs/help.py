1a2eb5ea11ffd269945914df6dd66f247541cdd9
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-11 12:36:36 +0900

    feat : Add help embed

--- cogs/help.py
+++ cogs/help.py
@@ -3,6 +3,8 @@ import asyncio
 from discord.ext.commands import Cog
 from discord.commands import slash_command, Option
 
+from config import KertColor, KertVer
+
 class Help(Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,6 +12,15 @@ class Help(Cog):
     @slash_command()
     async def help(self, ctx):
         """명령어들의 종류를 크게 구분해서 보여줄게요."""
+        helpEmbed = discord.Embed(title='도움말', description='여기서 사용할 수 있는 명령어 모음입니다.', color=KertColor)
+        helpEmbed.add_field(name='`/help`', value='컬트봇의 사용 방법을 알려줍니다.', inline=True)
+        helpEmbed.add_field(name='`/열렸나요`', value='컬방이 열려있는지 알려줍니다.', inline=True)
+        helpEmbed.add_field(name='`/열어주세요`', value='컬방 안의 사람들이 문을 열어주도록 합니다.', inline=True)
+        helpEmbed.add_field(name='`/지성이있나요`', value='요정이 컬방에 있는지 알려줍니다.', inline=True)
+        helpEmbed.add_field(name='`/개발진`', value='컬트봇의 개발진들을 보여줍니다.', inline=True)
+        helpEmbed.set_footer(text=KertVer)
+        
+        ctx.respond(embed=helpEmbed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ botName = 'KertBot'
 botID = ''
 botToken = ''
 EXTENSIONS = ['cogs.isopen', 'cogs.fairy', 'cogs.help']
+
+KertColor = 0xFFFFFF
+KertVer = '컬트봇 | V1.0'
