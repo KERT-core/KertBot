1847e39a40f7e7dbe672a94db8377c2e3a4da57c
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-17 21:01:23 +0900

    feat : Add kert wifi requests

--- cogs/fairy.py
+++ cogs/fairy.py
@@ -3,7 +3,10 @@ import asyncio
 from discord.ext.commands import Cog
 from discord.commands import slash_command
 
-fairyDevice = [r'2A:BC:84:94:87:9C', r'1C:C1:0C:E2:54:78', r'3E:57:55:B5:7A:8A', r'EE:A6:85:AE:31:2C', r'56:98:A2:57:F4:C3']
+import requests
+import json, base64
+
+fairyDevice = ['2A:BC:84:94:87:9C', '1C:C1:0C:E2:54:78', '3E:57:55:B5:7A:8A', 'EE:A6:85:AE:31:2C', '56:98:A2:57:F4:C3']
 
 class Fairy(Cog):
     def __init__(self, bot):
@@ -25,9 +28,36 @@ class Fairy(Cog):
         # 노트북:   1C:C1:0C:E2:54:78
         # 아이패드: 56:98:A2:57:F4:C3
         
-    
-        url = r'http://router.asus.com/Main_Login.asp'
+        with requests.Session() as session:
+            login_url = "http://router.asus.com/login.cgi"
+            
+            login_data = {
+                'action_wait': '5',
+                'current_page': 'Main_Login.asp',
+                'next_page': 'index.asp',
+                'login_authorization': 'S0VSVDpwYXNzd29yZDEyMw=='
+            }
+            login_headers = {
+                'Referer':'http://router.asus.com/Main_Login.asp',
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+            }
+        
+            login_res = session.post(login_url, headers=login_headers, data=login_data)
+            url = "http://router.asus.com/ajax_onboarding.asp"
+            headers = {
+                'Referer':'http://router.asus.com/index.asp',
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
+            }
+            
+            res = session.get(url, headers=headers)
+            res_text = res.text
+            res_text = res_text[res_text.rfind('get_allclientlist'):-36].replace('get_allclientlist = ', '')
+            res_json = json.loads(res_text)[0]['B0:6E:BF:66:07:80']
+            
+            
         kertList = []
+        kertList.extend(list(res_json['2G'].keys()))
+        kertList.extend(list(res_json['5G'].keys()))
         
         for adress in fairyDevice:
             if adress in kertList:
@@ -41,18 +71,3 @@ class Fairy(Cog):
 
 def setup(bot):
     bot.add_cog(Fairy(bot))
-    
-# import requests
-# from bs4 import BeautifulSoup
-
-# URL = 'http://router.asus.com/Main_Login.asp'
-# LOGIN_DATA = {
-#     'Username': 'Kert', 
-#     'Password': 'password123'
-# }
-
-# session = requests.session()
-# response = session.get(URL, data=LOGIN_DATA)
-# response.raise_for_status()
-
-# soup = BeautifulSoup(response.text, 'html.parser')
