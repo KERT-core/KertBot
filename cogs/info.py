30fb229433cb4dd531656c1cbaa2dd0db688c986
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-20 12:32:03 +0900

    feat : Merge credits -> info

--- cogs/info.py
+++ cogs/info.py
@@ -12,6 +12,7 @@ class Info(Cog):
     @slash_command(name='컬방')
     async def credits(self, ctx):
         """컬방에 대한 정보를 알려줍니다."""
+        
         kertEmbed = discord.Embed(title='컬방', description='', color=KertColor)
         kertEmbed.add_field(name='위치', value='중앙도서관 옆 정보화본부(110동) 110호', inline=False)
         kertEmbed.add_field(name='전화번호', value='053-950-6413', inline=False)
@@ -19,5 +20,28 @@ class Info(Cog):
         
         await ctx.respond(embed=kertEmbed)
         
+    @slash_command(name='개발진')
+    async def credits(self, ctx):
+        """컬트봇의 개발진들을 보여줍니다."""
+        
+        members = '''
+19 김다훈
+21 김기홍
+22 이지성
+22 정경호
+22 조은정
+       '''
+       
+        special_thanks = '''
+special thx to
+18 양태관, 21 이주형, 22 황지영
+       '''
+        
+        creditEmbed = discord.Embed(title='개발진', description='', color=KertColor)
+        creditEmbed.add_field(name=members, value=special_thanks)
+        creditEmbed.set_footer(text=KertVer)
+        
+        await ctx.respond(embed=creditEmbed)
+
 def setup(bot):
     bot.add_cog(Info(bot))
--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 botName = 'KertBot'
 botID = ''
-botToken = ''
+botToken = 'OTc2NzI3MzM5MjM5MTAwNDY2.GsOMed.k0dJn_b8auLsAxwycUu_q70HWK1cQcKqPTEef4'
 EXTENSIONS = ['cogs.help', 'cogs.isopen', 'cogs.fairy', 'cogs.info', 'cogs.credits']
+
+forbidenchannels=[546610833824940042, 699572422734643261, 855787966945034290, 572759629029834772]
 
 KertColor = 0x000000
 KertVer = '컬트봇 | V1.0'
