aa75e3434b9f419f978bdaca429b894371ad120c
cocoquiet <cocoquiet@knu.ac.kr>
2022-05-23 15:01:11 +0900

    feat : Add buzzer sound

--- cogs/open.py
+++ cogs/open.py
@@ -7,6 +7,9 @@ import asyncio
 from discord.ext.commands import Cog
 from discord.commands import slash_command, Option
 
+import RPI.GPIO as GPIO
+import time
+
 is_open = False
 
 class Open(Cog):
@@ -16,6 +19,7 @@ class Open(Cog):
     @slash_command(name='열렸나요')
     async def isopen(self, ctx):
         """컬방이 열려있는지 알려줍니다."""
+        
         if is_open:
             await ctx.respond('열렸어요')
         else:
@@ -24,7 +28,18 @@ class Open(Cog):
     @slash_command(name='열어주세요')
     async def openplz(self, ctx):
         """컬방 안의 사람들이 문을 열어주도록 합니다."""
-        pass
+        
+        buzzer = 13
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(buzzer, GPIO.OUT)
+        GPIO.setwarnings(False)
+        
+        PWM = GPIO.PWM(buzzer, 262)
+        PWM.start(50.0)
+        time.sleep(3)
+        
+        PWM.stop()
+        GPIO.cleanup()
 
 def setup(bot):
     bot.add_cog(Open(bot))
