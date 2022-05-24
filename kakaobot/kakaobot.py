f76e2a746f0ffb93630bf28fd045862e1fb9f5c8
Dasu <ekgns1106@naver.com>
2022-05-24 20:39:38 +0900

    made isopen function

--- kakaobot/kakaobot.py
+++ kakaobot/kakaobot.py
@@ -1,24 +1,44 @@
 from flask import Flask, request, jsonify
+import RPi.GPIO as GPIO
+import time
 
 application = Flask(__name__)
 
-@application.route("/")
+def isopen(self):
+    """컬방이 열려있는지 알려줍니다."""
+        
+    circuit = 7
+    cnt = 0
+        
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(circuit, GPIO.OUT)
+    GPIO.output(circuit, GPIO.LOW)
+        
+    time.sleep(0.1)
+    GPIO.setup(circuit, GPIO.IN)
+        
+    while GPIO.input(circuit) == GPIO.LOW and cnt < 11000:
+        cnt += 1
+        
+    if cnt < 10000:
+        return True
+    else:
+        return False
+            
+
+@application.route("/animal")
 def hello():
     return "Hello goorm!"
 
-@application.route("/",methods=['POST'])
+@application.route("/animal",methods=['POST'])
 def animal():
     req = request.get_json()
-    #조도 값이 200 이상이면 
-    isOpen = "열렸습니다"
-    #조도 값이 200 이하면
-    isClose = "닫혔습니다"
-    
-    light = 100
-    if light>=200:
-        answer = isOpen
+
+    jodo = isopen()
+    if jodo:
+        answer = "열렸습니다"
     else:
-        answer = isClose
+        answer = "닫혔습니다"
     # 답변 텍스트 설정
     res = {
         "version": "2.0",
