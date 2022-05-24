5cbc06b1086164410f22086277459ae196482ed6
hecoej <cheu03@naver.com>
2022-05-24 20:37:13 +0900

    feat : openplzif

--- cogs/open.py
+++ cogs/open.py
@@ -39,9 +39,24 @@ class Open(Cog):
     async def openplz(self, ctx):
         """컬방 안의 사람들이 문을 열어주도록 합니다."""
         
-        pygame.mixer.music.play()
-        await ctx.respond('컬방에 알림음을 울렸습니다.')
-
+        circuit = 7
+        cnt = 0
+        
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(circuit, GPIO.OUT)
+        GPIO.output(circuit, GPIO.LOW)
+        
+        time.sleep(0.1)
+        GPIO.setup(circuit, GPIO.IN)
+        
+        while GPIO.input(circuit) == GPIO.LOW and cnt < 11000:
+            cnt += 1
+            
+        if cnt < 10000:
+            pygame.mixer.music.play()
+            await ctx.respond('컬방에 알림음을 울렸습니다.')
+        else:
+            await ctx.respond('열어줄 사람이 없어요ㅠㅠ')
         
 def setup(bot):
     bot.add_cog(Open(bot))
