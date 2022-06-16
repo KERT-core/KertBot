9f65ea4143d26bd8aab96c18d55712eaf052aa6d
cocoquiet <cocoquiet@knu.ac.kr>
2022-06-17 03:13:24 +0900

    fix: Change bot token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 botName = 'KertBot'
 botID = 976727339239100466
-botToken = 'OTc2NzI3MzM5MjM5MTAwNDY2.GsOMed.k0dJn_b8auLsAxwycUu_q70HWK1cQcKqPTEef4'
+botToken = 'OTc2NzI3MzM5MjM5MTAwNDY2.Gt-GQ6.a3d7EgU8HnfGFiNu8DCcXTNzA9v3kyj26RgoOs'
 EXTENSIONS = ['cogs.help', 'cogs.open', 'cogs.fairy', 'cogs.info']
 
 KertColor = 0x000000
