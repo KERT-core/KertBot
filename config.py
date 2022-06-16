9df24d1cc4878d3fc29e48a2f115af2c77598cd2
cocoquiet <cocoquiet@knu.ac.kr>
2022-06-17 03:29:18 +0900

    feat: delete token file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import base64
 
 botName = 'KertBot'
 botID = 976727339239100466
-botToken = base64.b64decode(open('token', 'r').readline()).decode('ascii')
+botToken = base64.b64decode(b'T1RjMk56STNNek01TWpNNU1UQXdORFkyLkdGTVlOQy5QUjZJRkRsMmNGejRsMURRaW9peTJyN29nMWRyUS0wVVcwNVFxSQ==').decode('ascii')
 EXTENSIONS = ['cogs.help', 'cogs.open', 'cogs.fairy', 'cogs.info']
 
 KertColor = 0x000000
